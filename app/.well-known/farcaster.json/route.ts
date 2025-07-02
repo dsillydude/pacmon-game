d7031f9c1a8af7def78cca98ce6230740c1aa5c3
dsillydude <dsillydude@gmail.com>
2025-07-02 06:59:49 -0700

    Update farcaster.json with Pacmon details

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,22 +4,20 @@ import { APP_URL } from "../../../lib/constants";
 export async function GET() {
   const farcasterConfig = {
     accountAssociation: {
-       "header": "eyJmaWQiOjEwNTE4NTksInR5cGUiOiJhdXRoIiwia2V5IjoiMHg2NkY5MzA5OEI5ODc0MGY4MjhDZTgwOTk1ZTMwZDI0RGU1YzIwZGIzIn0",
-    "payload": "eyJkb21haW4iOiJwYWNtb24tZ2FtZS52ZXJjZWwuYXBwIn0",
-    "signature": "fRdHxO8GXSz7dJC9BoWny0twun68RdYvTMVhqIYZjZxC43I1bgTpyhM4ib8SpHSsDXZkjLEDg5EkSVl1BVFiPRw="
+      
     },
     frame: {
       version: "1",
-      name: "Monad Farcaster MiniApp Template",
-      iconUrl: `${APP_URL}/images/icon.png`,
+      name: "Pacmon - Monad Pacman Game", // <--- This needs to be updated
+      iconUrl: `${APP_URL}/images/splash.png`, // <--- This needs to be updated
       homeUrl: `${APP_URL}`,
       imageUrl: `${APP_URL}/images/feed.png`,
       screenshotUrls: [],
-      tags: ["monad", "farcaster", "miniapp", "template"],
-      primaryCategory: "developer-tools",
-      buttonTitle: "Launch Template",
+      tags: ["monad", "farcaster", "miniapp", "game", "pacman", "pacmon"], // <--- This needs to be updated
+      primaryCategory: "games", // <--- This needs to be updated
+      buttonTitle: "Play Pacmon", // <--- This needs to be updated
       splashImageUrl: `${APP_URL}/images/splash.png`,
-      splashBackgroundColor: "#ffffff",
+      splashBackgroundColor: "#200052", // <--- This needs to be updated
       webhookUrl: `${APP_URL}/api/webhook`,
     },
   };
