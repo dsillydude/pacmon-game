65173c0a2720886ed0fd97ecb0badb31503bead2
dsillydude <dsillydude@gmail.com>
2025-07-17 06:41:50 -0700

    fix codes

--- components/pages/app.tsx
+++ components/pages/app.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import PacmonGame from '@/components/PacmonGame'
+import PacmonGameImproved from '@/components/PacmonGameImproved'
 import { useFrame } from '@/components/farcaster-provider'
 import { SafeAreaContainer } from '@/components/safe-area-container'
 
@@ -32,7 +32,7 @@ export default function Home() {
 
   return (
     <SafeAreaContainer insets={context?.client.safeAreaInsets}>
-      <PacmonGame />
+      <PacmonGameImproved />
     </SafeAreaContainer>
   )
 }
